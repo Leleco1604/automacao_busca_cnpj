a6fbc3ea71ec9505902afbd5ea2fecb269b93d31
Leonardo Barbosa <87045040+Leleco1604@users.noreply.github.com>
2024-06-04 14:08:11 -0300

    finalizando um ex de interface grafica com SimpleGUi

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+from time import sleep 
 
 sg.theme('reddit')
 
@@ -9,7 +10,8 @@ tela_login = [
     [sg.Input(key= 'usuario')],
     [sg.Text('Senha')],
     [sg.Input(password_char='*', key= 'senha')],
-    [sg.Button('Enviar')]
+    [sg.Button('Enviar')],
+    [sg.Output(size =(43,10))]
 ]
 
 # Criar a janela 
@@ -18,4 +20,20 @@ janela = sg.Window('Login', layout = tela_login)
 #Ler os eventos 
 while True:
     event,values = janela.read()
+    if event == sg.WIN_CLOSED:
+        break
+    elif event == 'Enviar':
+        usurario_digitado = values['usuario']
+        senha_digitado = values['senha']
+
+        print('Passo 1 feito')
+        sleep(5)
+        print('Passo 2 feito')
+        sleep(5)
+        print('Passo 3 feito')
+        sleep(5)
+
+        
+
+
     
