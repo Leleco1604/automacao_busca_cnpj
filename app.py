269df15244f51815271f29ed8cc7559ea7e7c05b
Leonardo Barbosa <87045040+Leleco1604@users.noreply.github.com>
2024-06-03 21:41:07 -0300

    tela login

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 import PySimpleGUI as sg
 
 sg.theme('reddit')
+
+#Layout 
+
+tela_login = [
+    [sg.Text('Usuário')],
+    [sg.Input(key= 'usuario')],
+    [sg.Text('Senha')],
+    [sg.Input(password_char='*', key= 'senha')],
+    [sg.Button('Enviar')]
+]
+
+#
